60fe0418e6de1a24333223a8a8905214d7e503fa
Matt Russell <mrussell@cs.tufts.edu>
2019-10-01 20:14:10 -0400

    updated ubuntu version

--- screen.h
+++ screen.h
@@ -1,5 +1,5 @@
 #ifndef SCREENH
 #define SCREENH
-constexpr auto screenWidth = 800;
-constexpr auto screenHeight = 800;
+constexpr auto screenWidth = 400;
+constexpr auto screenHeight = 400;
 #endif
